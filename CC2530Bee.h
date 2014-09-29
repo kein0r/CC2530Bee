e33eaad54aba1efba0f7d4c4060a3cf213051cb0
kein0r <github@dummgebabbel.de>
2014-09-29 07:44:44 +0000

    Added init function for USART Added first version of global configuration variable

--- CC2530Bee.h
+++ CC2530Bee.h
@@ -10,6 +10,20 @@
 /*******************| Macros |*****************************************/
 
 /*******************| Type definitions |*******************************/
+typedef enum {
+  CC2530Bee_SleepMode_None;
+} CC2530Bee_SleepMode_t;
+
+/**
+ * \brief Struct to store all kind of configuration data for CC2530Bee.
+ * This variable will be stored in EEPROM
+ */
+typedef struct {
+  USART_Baudtate_t USART_Baudrate;
+  CC2530Bee_SleepMode_t sleepMode;
+  uint16_t crc;                 /*!< crc to be saved in EEPROM to check if data is valid */
+} CC2530Bee_Config_t;
+
 
 /*******************| Global variables |*******************************/
 
